ea70e3d4388a3623a8f66f99133c4e640d751c96
Bhumi Davra <bhumidavra99@gmail.com>
2025-11-12 22:29:34 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { BrowserRouter } from 'react-router-dom';
+import AllRoutes from './routes/AllRoutes';
+
+function App() {
+  
+
+  return (
+     <BrowserRouter>
+     <AllRoutes/>
+     </BrowserRouter>
+  )
+}
+
+export default App
